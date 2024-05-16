f07b074bc7d9764dcc0fd58e31cfdeba2dab67cc
root <root@tula-term.djue.local>
2024-05-16 19:43:21 +0000

    feat: added all files

---
+++ scripts/install_eslint_packages.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+yarn add -D eslint-plugin-regexp \
+  eslint-plugin-optimize-regex \
+  eslint-plugin-lodash \
+  eslint-config-airbnb-base \
+  eslint-config-google \
+  eslint-config-prettier \
+  eslint-plugin-eslint-comments \
+  eslint-plugin-import \
+  eslint-plugin-jsonc \
+  eslint-plugin-jsx-a11y \
+  eslint-plugin-lodash \
+  eslint-plugin-no-secrets \
+  eslint-plugin-no-unsanitized \
+  eslint-plugin-nuxt \
+  eslint-plugin-promise \
+  eslint-plugin-react \
+  eslint-plugin-security \
+  eslint-plugin-sonarjs \
+  eslint-plugin-unicorn \
+  eslint-plugin-vue \
+  eslint-plugin-xss \
+  @typescript-eslint/eslint-plugin \
+  @typescript-eslint/parser
+  # @nuxtjs/eslint-config-typescript \
+  # @nuxtjs/eslint-module
+
+# @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint typescript
